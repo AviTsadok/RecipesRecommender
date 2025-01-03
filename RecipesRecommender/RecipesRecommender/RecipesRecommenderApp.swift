a8440843a2e49c1e1b92f2cf9661f144ddc87e07
Avi Tsadok <avi.tsadok@melio.com>
2025-01-03 06:45:39 +0200

    First push

---
+++ RecipesRecommender/RecipesRecommender/RecipesRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesRecommenderApp.swift
+//  RecipesRecommender
+//
+//  Created by Avi Tsadok on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
